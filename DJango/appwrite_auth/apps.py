1cf9165cffbbff95b5bda730a19da24a06527e0f
swapnil ingle <ingleswapnil2004@gmail.com>
2025-02-02 22:22:08 +0530

    Remove unused Django app files and templates

--- DJango/appwrite_auth/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppwriteAuthConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'appwrite_auth'
--- DJango/appwrite_auth/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render  # type: ignore
-
-# Create your views here.
-
-def login(request):
-    return render(request, 'appwrite_auth/login.html')
-
-def signup(request):
-    return render(request, 'appwrite_auth/signup.html')
